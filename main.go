3a702778e69c8e15c21eba41200fa80f4bcfda61
Chris Frewin <frewin.christopher@gmail.com>
2023-05-17 11:54:46 +0200

    main.go mostly implemented!

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"allergycron/allergy_api"
+	"allergycron/utils"
+	"log"
 	"time"
 
 	"github.com/robfig/cron"
@@ -14,5 +17,22 @@ func main() {
 
 	cronJob := cron.NewWithLocation(loc)
 	cronJob.AddFunc("0 0 8 * * *", func() {
+		dailyAverageMessage, err := allergy_api.GetHourlyLoadData()
+		if err != nil {
+			panic(err)
+		}
+
+		historicalAverageMessage, err := allergy_api.GetCurrentChartData()
+		if err != nil {
+			panic(err)
+		}
+
+		slackMessage := *dailyAverageMessage + "\n" + *historicalAverageMessage
+		err = utils.SendSlackMessage(slackMessage)
+		if err != nil {
+			panic(err)
+		}
+
+		log.Println("Successfully sent Slack message: " + slackMessage)
 	})
 }
